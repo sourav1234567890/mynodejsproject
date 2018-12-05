b0ed9aa01c677389cb733ce2e264788326aa2c03
sourav <souravsingh7389@gmail.com>
2018-12-06 00:13:20 +0530

    dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,25 +2,31 @@
 module.exports={
 dashboard: (req, res) => {
 	var sessData = req.session;
-    console.log(sessData);
+    //console.log("hhhhhhh
+
 	if(sessData.email1){
+        let userimage=sessData.userimage1;
+        console.log("userimage"+userimage);
         let logout="logout"
         res.render('dashboard.ejs', {
             title: "Welcome to app | dashboard page"
             ,message: '',
             session:sessData,
             logout:logout,
-            login:''
+            login:'',
+            userimage:userimage
         });
     }else{
     	//res.redirect('/login');
         let login="login"
+        let userimage="";
         //let logout=""
         res.render('dashboard.ejs', {
             title: "Welcome to app | dashboard page"
             ,message: '',
             logout:'',
-            login:login
+            login:login,
+            userimage:userimage
         });
     }
     },
